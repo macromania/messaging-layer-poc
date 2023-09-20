1b93319ddccc729fbad12bb36970de4f8f45eeb8
Mahmut Canga <cangamahmut@gmail.com>
2023-09-20 13:24:09 +0100

    init

---
+++ messaging-poc-export/src/main/java/com/backbase/dataverse/messagingpocexport/MessagingPocExportApplication.java
@@ -0,0 +1,13 @@
+package com.backbase.dataverse.messagingpocexport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingPocExportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingPocExportApplication.class, args);
+    }
+
+}
---
+++ messaging-poc-export/src/test/java/com/backbase/dataverse/messagingpocexport/MessagingPocExportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backbase.dataverse.messagingpocexport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingPocExportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
