29ffe48454d33c176d23bfca682b16d80a4e1350
Mahmut Canga <cangamahmut@gmail.com>
2023-09-26 14:01:26 +0100

    increase test message count to ~13K

--- load-testing/script.js
+++ load-testing/script.js
@@ -2,8 +2,8 @@ import http from "k6/http";
 import { sleep } from "k6";
 
 export const options = {
-    vus: 10,
-    duration: "30s",
+    vus: 100,
+    duration: "3m",
 };
 
 export default function () {
